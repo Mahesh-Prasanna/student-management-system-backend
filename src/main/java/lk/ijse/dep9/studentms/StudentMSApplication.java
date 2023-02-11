29d5ef716b2201c7fff3c6c82546d5be9737a91c
Mahesh Prasanna <damp.dassanayake@gmail.com>
2023-02-11 16:47:02 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/dep9/studentms/StudentMSApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep9.studentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/dep9/studentms/AdlApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep9.studentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
