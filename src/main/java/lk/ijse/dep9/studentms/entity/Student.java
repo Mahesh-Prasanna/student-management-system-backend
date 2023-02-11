5e9ba2185101448d0e5d0f4c22dac10f684c2b4c
Mahesh Prasanna <damp.dassanayake@gmail.com>
2023-02-11 17:11:36 +0530

    Create Entity Class

--- src/main/java/lk/ijse/dep9/studentms/entity/Student.java
+++ src/main/java/lk/ijse/dep9/studentms/entity/Student.java
@@ -1,4 +1,22 @@
 package lk.ijse.dep9.studentms.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
+    @Id
+    private int id;
+    private String name;
+    private String address;
+    private String contact;
+
 }
